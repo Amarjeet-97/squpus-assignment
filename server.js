395bd2612a516f261f38ddc6a5f2d1cdfe971520
Amarjeet-97 <106522356+Amarjeet-97@users.noreply.github.com>
2023-04-01 17:13:31 +0530

    change in server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server')
+const auth= require('json-server-auth')
+
 const server = jsonServer.create()
 const router = jsonServer.router('Endpoints.json')
-const middlewares = jsonServer.defaults()
- 
-server.use(middlewares)
-server.use('', router)
+// const middlewares = "./node_modules/json-server-auth"
+server.Endpoints=router.Endpoints
+server.use(auth)
+server.use(router)
 server.listen(process.env.PORT || 8080, () => {
   console.log('JSON Server is running')
 })
